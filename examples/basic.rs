496a8f5e8fe39fafbbdb3f0bd79778fb0bb946a6
medzernik <medzernik1@gmail.com>
2023-02-08 19:59:50 +0100

    omfg

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,5 +3,7 @@ use window_titles::{Connection, ConnectionTrait};
 fn main() {
 	let connection = Connection::new().unwrap();
 	connection.window_titles().unwrap().into_iter()
-		.for_each(|title| println!("{}", title))
+		.for_each(|(id, title)| println!("FINAL RESULT:\n id: {:?}, title: {:?}", id, title));
+
+		println!("test");
 }
--- src/apple.rs
+++ src/apple.rs
@@ -1,23 +1,36 @@
-use std::{error::Error, fmt, process::Command};
+use std::{error::Error, fmt, process::{Command, Output}};
 
 use crate::{ConnectionTrait, Result};
 
-const PREFIX: &str = r#"tell application "System Events""#;
-const SUFFIX: &str = r#"to get the title of every window of every process"#;
+const PREFIX_ID: &str = r#"tell application "System Events""#;
+const SUFFIX_ID: &str = r#"to get the unix id of every process"#;
+
+const PREFIX_WINDOW: &str = r#"tell application "System Events""#;
+const SUFFIX_WINDOW: &str = r#"to get the title of every window of every process"#;
+
 const PERMISSION_ERROR: &str = "osascript is not allowed assistive access";
 
 pub struct Connection;
 impl ConnectionTrait for Connection {
 	fn new() -> Result<Self> { Ok(Self) }
 	fn window_titles(&self) -> Result<Vec<(u32,String)>> {
-		let arguments = &["-ss", "-e", &format!("{} {}", PREFIX, SUFFIX)];
-		let command = Command::new("osascript").args(arguments).output()
+		let arguments = &["-ss", "-e", &format!("{} {}", PREFIX_ID, SUFFIX_ID)];
+		let command_pid = Command::new("osascript").args(arguments).output()
 			.expect("failed to execute AppleScript command");
+			println!("OUTPUT PID: {:?}", command_pid);
 
-		let error = String::from_utf8_lossy(&command.stderr);
-		match error.contains(PERMISSION_ERROR) {
-			true => Err(WindowTitleError::NoAccessibilityPermission.into()),
-			false => Ok(split(&String::from_utf8_lossy(&command.stdout))),
+		let arguments = &["-ss", "-e", &format!("{} {}", PREFIX_WINDOW, SUFFIX_WINDOW)];
+		let command_window = Command::new("osascript").args(arguments).output()
+			.expect("failed to execute AppleScript command");
+			println!("OUTPUT WINDOWS: {:?}", command_window);
+
+		
+		
+		let error_window = String::from_utf8_lossy(&command_window.stderr);
+		let error_pid = String::from_utf8_lossy(&command_pid.stderr);
+		match (error_window.contains(PERMISSION_ERROR), error_pid.contains(PERMISSION_ERROR)) {
+			(false, false) => Ok(split(&String::from_utf8_lossy(&command_window.stdout), &String::from_utf8_lossy(&command_pid.stdout))),
+			(_, _) => Err(WindowTitleError::NoAccessibilityPermission.into()),
 		}
 	}
 }
@@ -35,13 +48,18 @@ impl fmt::Display for WindowTitleError {
 }
 impl Error for WindowTitleError {}
 
-fn split(mut string: &str) -> Vec<(u32,String)> {
+fn split(mut string: &str, mut pid: &str) -> Vec<(u32,String)> {
 	let mut titles = Vec::new();
-	while let Some(start) = string.find('"') {
+	while let (Some(start), Some(start_pid)) = (string.find('"'), pid.find(", ")) {
 		let end = string[start + 1..].find('"').unwrap();
-		titles.push((0,string[start + 1..][..end].to_string()));
+		let end_pid = pid[start_pid + 1..].find(", ").unwrap();
+		titles.push((pid[start_pid + 1..][..end_pid].to_string().trim().parse::<u32>().unwrap(),string[start + 1..][..end].to_string()));
+		
 		string = &string[start + 1..][end + 1..];
+		pid = &pid[start_pid + 1..][end_pid + 1..];
 	}
+	println!("titles: {:?}", titles);
+	
 	titles
 }
 
